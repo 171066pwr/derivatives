878aa390158a574ad21676f9dcbf45a663671319
171066pwr <171066@student.pwr.edu.pl>
2025-01-25 00:07:26 +0100

    Power tests

--- test/PowerTest.cpp
+++ test/PowerTest.cpp
@@ -29,20 +29,23 @@ void PowerTest::testEqualsOperators() {
 }
 
 void PowerTest::testEvaluation() {
-    BaseEntity *scalar = new Scalar(3);
-    BaseEntity *variable = new Variable(2);
-    BaseEntity *sum = new Sum(1, {variable, scalar});
+    BaseEntity *zero = new Scalar(0);
+    BaseEntity *one = new Scalar();
+    BaseEntity *scalar3 = new Scalar(3);
+    BaseEntity *variable = new Variable();
+    BaseEntity *variable2 = new Variable(2);
+    BaseEntity *sum = new Sum(1, {variable2, scalar3});
     BaseEntity *testEntity = new Power(2);
-    BaseEntity *multi = new Multiplication(2, {variable->copy(), scalar->copy()});
+    BaseEntity *multi = new Multiplication(2, {variable2->copy(), scalar3->copy()});
     BaseEntity *expected;
     double x = 2.0;
-    Logger::important("Testing constructors and toString");
+    Logger::important("Testing evaluation");
 
-    testEntity->addElement(scalar->copy());
+    testEntity->addElement(scalar3->copy());
     testEntity = printAndEvaluateFunction(testEntity, "Scalar base");
     testValue(testEntity, expected = new Scalar(9));
 
-    replace(testEntity, new Power(3, {variable->copy()}, 2));
+    replace(testEntity, new Power(3, {variable2->copy()}, 2));
     testEntity = printAndEvaluateFunction(testEntity, "Variable base");
     replace(testEntity, printAndEvaluateValue(testEntity, x));
     replace(expected, new Scalar(128));
@@ -60,19 +63,56 @@ void PowerTest::testEvaluation() {
     replace(expected, new Scalar(191581231380566414401.0));
     testValue(testEntity, expected);
 
-    BaseEntity *power = new Power(variable->copy(), variable->copy(), 0.25);
-    replace(testEntity, new Power(power->copy(), power->copy(), 2));
+    BaseEntity *power = new Power(variable2->copy(), variable2->copy());
+    replace(testEntity, new Power(scalar3->copy(), power->copy()));
+    testEntity = printAndEvaluateFunction(testEntity, "Power base, scalar power");
+    replace(testEntity, printAndEvaluateValue(testEntity, x));
+    replace(expected, new Scalar(16777216.0));
+    testValue(testEntity, expected);
+
+    replace(testEntity, new Power(variable2->copy(), power->copy()));
+    testEntity = printAndEvaluateFunction(testEntity, "Power base, variable power");
+    replace(testEntity, printAndEvaluateValue(testEntity, x));
+    replace(expected, new Scalar(4294967296.0));
+    testValue(testEntity, expected);
+
+    replace(testEntity, new Power(new Power(variable->copy(), variable->copy()), power->copy()));
     testEntity = printAndEvaluateFunction(testEntity, "Power base, power power - without multipliers");
     replace(testEntity, printAndEvaluateValue(testEntity, x));
-    replace(expected, new Scalar(191581231380566414401.0));
+    replace(expected, new Scalar(4294967296.0));
     testValue(testEntity, expected);
 
-    replace(power, new Power(variable->copy(), variable->copy(), 0.25));
+    replace(power, new Power(variable2->copy(), variable2->copy(), 0.25));
     replace(testEntity, new Power(power->copy(), power->copy(), 2));
     testEntity = printAndEvaluateFunction(testEntity, "Power base, power power");
-    replace(testEntity, printAndEvaluateValue(testEntity, x));
-    replace(expected, new Scalar(191581231380566414401.0));
+    replace(testEntity, printAndEvaluateValue(testEntity, 1));
+    replace(expected, new Scalar(8.0));
     testValue(testEntity, expected);
 
-    deleteMultiple({sum, testEntity, multi});
+    replace(testEntity, new Power(zero->copy(), zero->copy()));
+    testEntity = printAndEvaluateFunction(testEntity, "Zero base, zero power");
+    replace(testEntity, printAndEvaluateValue(testEntity, 1));
+    testValue(testEntity, one);
+
+    replace(testEntity, new Power(zero->copy(), scalar3->copy()));
+    testEntity = printAndEvaluateFunction(testEntity, "3 base, zero power");
+    replace(testEntity, printAndEvaluateValue(testEntity, 1));
+    testValue(testEntity, one);
+
+    replace(testEntity, new Power(zero->copy(), variable2->copy()));
+    testEntity = printAndEvaluateFunction(testEntity, "variable base, zero power");
+    replace(testEntity, printAndEvaluateValue(testEntity, 2));
+    testValue(testEntity, one);
+
+    replace(testEntity, new Power(scalar3->copy(), zero->copy()));
+    testEntity = printAndEvaluateFunction(testEntity, "zero base, 3 power");
+    replace(testEntity, printAndEvaluateValue(testEntity, 2));
+    testValue(testEntity, zero);
+
+    replace(testEntity, new Power(scalar3->copy(), one->copy()));
+    testEntity = printAndEvaluateFunction(testEntity, "1 base, 3 power");
+    replace(testEntity, printAndEvaluateValue(testEntity, 2));
+    testValue(testEntity, one);
+
+    deleteMultiple({sum, testEntity, multi, zero, one, variable});
 }
